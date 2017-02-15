8125f36bc156f79d1fe87dd8a7187136abea56c9
mykolak <mykola.kolisnyk@thetrainline.com>
2017-02-15 13:11:00 +0000

    Data-driven test was created

--- UITesting/Test.cs
+++ UITesting/Test.cs
@@ -29,20 +29,33 @@ namespace UITesting
 		{
 			driver.Quit();
 		}
-		[Test()]
-		public void TestValidSearch()
+		static Object[] SearchData =
+		{
+			new Object[] {"London", true, 1},
+			new Object[] {"Manchester", false, 2},
+		};
+		[Test(), TestCaseSource("SearchData")]
+		public void TestValidSearch(String destination, Boolean isLeisure, int adults)
 		{
 			driver.FindElement(By.Id("ss")).Click();
 			driver.FindElement(By.Id("ss")).Clear();
-			driver.FindElement(By.Id("ss")).SendKeys("London");
+			driver.FindElement(By.Id("ss")).SendKeys(destination);
 			//System.Threading.Thread.Sleep(1000);
 			driver.FindElement(By.XPath("(//li[contains(@class, 'autocomplete__item')])[1]")).Click();
 			//System.Threading.Thread.Sleep(1000);
 			driver.FindElement(By.CssSelector("i.sb-date-field__chevron.bicon-downchevron")).Click();
 			driver.FindElement(By.XPath("//table[@class='c2-month-table']//td[contains(@class, 'c2-day-s-today')]")).Click();
-			driver.FindElement(By.XPath("(//input[@name='sb_travel_purpose'])[2]"))
-				.Click();
-			new SelectElement(driver.FindElement(By.Id("group_adults"))).SelectByText("1");
+			if (isLeisure)
+			{
+				driver.FindElement(By.XPath("(//input[@name='sb_travel_purpose'])[2]"))
+					.Click();
+			}
+			else 
+			{ 
+				driver.FindElement(By.XPath("(//input[@name='sb_travel_purpose'])[1]"))
+					.Click();
+			}
+			new SelectElement(driver.FindElement(By.Id("group_adults"))).SelectByText("" + adults);
 			driver.FindElement(By.XPath("//button[@type='submit']")).Click();
 			driver.FindElement(By.Id("ss")).Click();
 		}
